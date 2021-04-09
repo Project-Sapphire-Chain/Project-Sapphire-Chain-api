f4ccc3e911de8c339dc14d02a4b6b42ce699c708
Shalin Desai <desai.381@osu.edu>
2021-04-09 14:10:24 -0400

    main

--- backend/Project-Sapphire-Chain.api/Controllers/CatalogController.cs
+++ backend/Project-Sapphire-Chain.api/Controllers/CatalogController.cs
@@ -11,8 +11,50 @@ namespace Sapphire.Chain.Api.Controllers
 		[HttpGet]
 		public IActionResult GetItems()
 	{
-		return Ok("hello world.");
+		var items = new List<Item>()
+		{
+			new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+			new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+		};
+
+		return Ok(items);
 	}
-	}
+	 	[HttpGet("{id:int}")]
+		public IActionResult GetItem(int id)
+		{
+			var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+			item.Id = id;
+
+			return Ok(item);
+		}
+
+		[HttpPost]
+		public IActionResult Post(Item item)
+		{
+			return Created("/catalog/42",item);
+		}
+
+		[HttpPost("{id:int}/ratings")]
+		public IActionResult PostRating(int id, [FromBody] Rating rating)
+		{
+			var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+			item.Id=id;
+			item.AddRating(rating);
+
+			return Ok(item);
+		}
+
+		[HttpPut("{id:int}")]
+		public IActionResult PutItem(int id, [FromBody] Item item)
+		{
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteItem(int id)
+		{
+			return Ok();
+		}
+}
 }
 
