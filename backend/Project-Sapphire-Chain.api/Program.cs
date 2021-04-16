54dc51d3f810057c3fe18a5e375faded51ce898b
Shalin Desai <desai.381@osu.edu>
2021-04-16 10:44:56 -0400

    main

--- backend/Project-Sapphire-Chain.api/Program.cs
+++ backend/Project-Sapphire-Chain.api/Program.cs
@@ -4,17 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Project.Sapphire.Chain.Data;
 
 namespace Project_Sapphire_Chain.api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+        private static void CreateDbIfNotExists(IHost host){
+
+        using (var scope = host.Services.CreateScope()){
+        var services = scope.ServiceProvider;
+        var logger = services.GetRequiredService<ILogger<Program>>();
+
+        try{
+            var context = services.GetRequiredService<StoreContext>();
+            context.Database.EnsureCreated();
+            DbInitializer.Initialize(context, logger);
         }
+        catch(Exception ex){
+            logger.LogError(ex, "Error occured creating the database.");
+        }
+        }
+        }
+        public static void Main(string[] args){
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+            }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
