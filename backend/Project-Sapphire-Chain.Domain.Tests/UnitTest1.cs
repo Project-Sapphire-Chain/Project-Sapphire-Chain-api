e52ac92c8996265e712dd2adf373d18452b48ec7
BenPrazak <78315067+BenPrazak@users.noreply.github.com>
2021-04-25 15:29:58 -0400

    main

---
+++ backend/Project-Sapphire-Chain.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project_Sapphire_Chain.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
